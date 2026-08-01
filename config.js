2a318f586bff4cb2cb700a2ec8224f2af7c678fa
sakshamvermaa124-dotcom <sakshamverma124@gmail.com>
2026-08-02 03:56:34 +0530

    fix: Always point local frontend to live backend so emails and auth work without local python server

--- config.js
+++ config.js
@@ -16,6 +16,7 @@
     window.location.hostname.startsWith('192.168.')
   );
 
-  window.SKILLME_API      = isLocal ? LOCAL_URL : RENDER_URL;
-  window.SKILLME_FRONTEND = isLocal ? 'http://localhost:8080' : window.location.origin;
+  // Force local development to use the live API so you don't need to run the Python server locally
+  window.SKILLME_API      = RENDER_URL;
+  window.SKILLME_FRONTEND = isLocal ? 'http://127.0.0.1:5500' : window.location.origin;
 })();
